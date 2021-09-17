afe9edbf4a0128ba91b3c6da8d700cca44b22b8b
Vigneshraja G <vigneshraja20@gmail.com>
2021-09-17 17:46:44 +0530

    @axe-core/react configuration

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
 
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('@axe-core/react')
+  axe(React, ReactDOM, 1000)
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
