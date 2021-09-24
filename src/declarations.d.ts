f7a08a5bc647bed3e704d9db546fda00dcb69e17
Vigneshraja G <vigneshraja20@gmail.com>
2021-09-24 23:26:06 +0530

    i18n type fix

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -4,3 +4,8 @@ declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
   export default content
 }
+
+declare module 'i18next' {
+  import { default as i18n } from 'i18next'
+  export default i18n
+}
