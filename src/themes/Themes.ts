cc0922e94b3f4ed7bb94a04ce480352aff5055c6
Vigneshraja G <vigneshraja20@gmail.com>
2021-09-24 23:22:23 +0530

    Theme color scheme config

--- src/themes/Themes.ts
+++ src/themes/Themes.ts
@@ -14,3 +14,30 @@ export const getFontSize = style('textZoom', {
   normal: '1em',
   magnify: '1.2em',
 })
+
+const themeScheme = {
+  dark: {
+    background: '#1c4958',
+    foreground: '#a1ccde',
+
+    accent1: '#f4c8a5',
+    accent2: '#b47368',
+    accent3: '#923621',
+
+    text1: '#c6cfd3',
+    text2: '#808186',
+  },
+  light: {
+    background: '#a53237',
+    foreground: '#f66f40',
+
+    accent1: '#f8986d',
+    accent2: '#9c4952',
+    accent3: '#f66f40',
+
+    text1: '#f5e5e1',
+    text2: '#354f55',
+  },
+}
+
+export default themeScheme
--- src/themes/useTheme.ts
+++ src/themes/useTheme.ts
@@ -1,20 +1,31 @@
 import { useState, useEffect } from 'react'
 import storage from 'local-storage-fallback'
+import themeScheme from './Themes'
 
 export default function useTheme(
   defaultTheme = { mode: 'light', textZoom: 'normal' }
 ) {
   const [theme, _setTheme] = useState(getInitialTheme)
-
+  console.log(theme)
   function getInitialTheme() {
     const savedTheme = storage.getItem('theme')
-    return savedTheme ? JSON.parse(savedTheme) : defaultTheme
+    if (savedTheme) {
+      const theme = JSON.parse(savedTheme)
+      theme.scheme = themeScheme[theme.mode]
+      return theme
+    } else {
+      defaultTheme.scheme = themeScheme['light']
+      return defaultTheme
+    }
+    // return savedTheme ? JSON.parse(savedTheme) : defaultTheme
   }
 
   useEffect(() => {
+    delete theme.scheme
     storage.setItem('theme', JSON.stringify(theme))
   }, [theme])
 
+  theme.scheme = themeScheme[theme.mode]
   return {
     ...theme,
     setTheme: ({ ...theme }) => _setTheme(theme),
